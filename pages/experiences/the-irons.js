9b2c63890884be400588ae933d91126f47fe131f
tarek5142-godaddy <tarek5142@godaddy.com>
2025-06-25 19:15:50 -0500

    updated irons experience

--- pages/experiences/the-irons.js
+++ pages/experiences/the-irons.js
@@ -3,6 +3,8 @@ import { Bangers } from 'next/font/google';
 import { Patrick_Hand } from 'next/font/google';
 import { useRef, useEffect } from "react";
 import { gsap } from "gsap";
+import Starfield from "/components/Starfield.tsx";
+
 
 const bangers = Bangers({
     subsets: ['latin'],
@@ -17,6 +19,8 @@ const patrickHand = Patrick_Hand({
 export default function TheIrons() {
 
     const marqueeRef = useRef(null);
+    const peterClearBoyRef = useRef(null);
+    const gilbertGreenRef = useRef(null);
 
     useEffect(() => {
         const marquee = marqueeRef.current;
@@ -26,16 +30,28 @@ export default function TheIrons() {
         marquee.innerHTML += marquee.innerHTML;
 
         gsap.to(marquee, {
-        x: 0,
-        duration: 20,
-        ease: "linear",
-        repeat: -1,
-        modifiers: {
-            x: gsap.utils.unitize(x => (parseFloat(x) % totalWidth))
-        }
+            x: 0,
+            duration: 20,
+            ease: "linear",
+            repeat: -1,
+            modifiers: {
+                x: gsap.utils.unitize(x => (parseFloat(x) % totalWidth))
+            }
         });
 
         gsap.set(marquee, { x: -totalWidth });
+
+        gsap.set(peterClearBoyRef.current, { scale: 0 });
+
+        let tl = gsap.timeline({ repeat: -1, repeatDelay: 2 });
+
+        tl.to(peterClearBoyRef.current, {
+            duration: 8,
+            rotate: 360,
+            scale: 1,
+            ease: "linear",
+            repeat: 0,
+        })
 
     }, []);
 
@@ -46,7 +62,13 @@ export default function TheIrons() {
                 <title>The Hero's Journey</title>
             </Head>
             <main>
-                <section className="w-full h-auto bg-[#141312]">
+                <section className="w-full h-auto">
+                    <Starfield
+                        starCount={2000}
+                        starColor={[255, 255, 255]}
+                        speedFactor={0.05}
+                        backgroundColor="black"
+                    />
 
                     <div className="overflow-hidden w-full h-24 relative">
                         <div
@@ -54,18 +76,20 @@ export default function TheIrons() {
                             className="absolute top-0 left-0 flex gap-2 items-center h-full"
                         >
                             {Array.from({ length: 10 }).map((_, i) => (
-                            <img
-                                key={i}
-                                src="/images/the-irons/irons-logo.png"
-                                alt="The Irons Logo"
-                                className="h-22 w-auto object-contain"
-                            />
+                                <img
+                                    key={i}
+                                    src="/images/the-irons/irons-logo.png"
+                                    alt="The Irons Logo"
+                                    className="h-22 w-auto object-contain"
+                                />
                             ))}
                         </div>
                     </div>
 
+                    <img ref={peterClearBoyRef} className="w-[65%] mx-auto mt-8" src="/images/the-irons/peter-clear-boy.svg" alt="Peter Clear Boy" />
+
                     <div>
-                        <h1 className={`${bangers.className} text-6xl text-center text-[#EFA111] pt-16`}>The Hero's Journey</h1>
+                        <h1 className={`${bangers.className} text-6xl text-center text-[#EFA111] pt-4`} style={{ textShadow: '6px 0 0 #A7201D', WebkitTextStroke: '1px black' }}>The Hero's Journey</h1>
                         <h6 className={`${patrickHand.className} text-center text-gray-50`}>A fan project by Tarek Zaher</h6>
                     </div>
 
