709bd195261e3e8ca495aadad33240e170353777
tarek5142-godaddy <tarek5142@godaddy.com>
2025-07-06 22:55:12 -0500

    fine tune mobile homepage

--- pages/index.js
+++ pages/index.js
@@ -24,33 +24,33 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section>
-        <div className={`bg-[url(/images/beautiful_clouds.png)] bg-cover bg-center h-[40vh] ${utilStyles.bentoRectangle}`}></div>
+        <div className={`bg-[url(/images/beautiful_clouds.png)] bg-cover bg-center h-[250px] ${utilStyles.bentoRectangle}`}></div>
         
         <div className={`bg-[#392F2D] ${utilStyles.bentoRectangle}`}>
           <h3 className={`text-white p-8 text-2xl ${libreBaskervilleBold.className}`}>I'm</h3>
           <h3 className={`text-white text-center text-8xl px-4 my-4 ${libreBaskervilleBold.className} h-auto`}>Tarek</h3>
         </div>
 
-        <h2 className={`m-[20px] p-4 text-black text-xl ${libreBaskervilleRegular.className}`}>I'm passionate about creating engaging web experiences and appreciating the beauty of the world around us.</h2>
+        <h2 className={`m-[20px] px-4 pt-4 pb-0 text-black text-xl ${libreBaskervilleRegular.className}`}>I'm passionate about creating engaging web experiences and appreciating the beauty of the world around us.</h2>
         
         <div className="flex items-center justify-center">
-          <div className="grid grid-cols-2 grid-rows-2 gap-5">
-            <svg width="120" height="120" viewBox="0 0 120 120" className="block">
-              <path d="M60,0 A60,60 0 1,0 60,120 A60,60 0 1,0 60,0" fill="#95B8D1" />
+          <div className="grid grid-cols-4 grid-rows-1 gap-1">
+            <svg width="80" height="80" viewBox="0 0 80 80" className="block">
+              <path d="M40,0 A40,40 0 1,0 40,80 A40,40 0 1,0 40,0" fill="#95B8D1" />
             </svg>
-            <svg width="120" height="120" viewBox="0 0 120 120" className="block">
-              <path d="M60,0 A60,60 0 1,0 60,120 A60,60 0 1,0 60,0" fill="#8A9B68" />
+            <svg width="80" height="80" viewBox="0 0 80 80" className="block">
+              <path d="M40,0 A40,40 0 1,0 40,80 A40,40 0 1,0 40,0" fill="#8A9B68" />
             </svg>
-            <svg width="120" height="120" viewBox="0 0 120 120" className="block">
-              <path d="M60,0 A60,60 0 1,0 60,120 A60,60 0 1,0 60,0" fill="#D7816A" />
+            <svg width="80" height="80" viewBox="0 0 80 80" className="block">
+              <path d="M40,0 A40,40 0 1,0 40,80 A40,40 0 1,0 40,0" fill="#D7816A" />
             </svg>
-            <svg width="120" height="120" viewBox="0 0 120 120" className="block">
-              <path d="M60,0 A60,60 0 1,0 60,120 A60,60 0 1,0 60,0" fill="#726675" />
+            <svg width="80" height="80" viewBox="0 0 80 80" className="block">
+              <path d="M40,0 A40,40 0 1,0 40,80 A40,40 0 1,0 40,0" fill="#726675" />
             </svg>
           </div>
         </div>
 
-        <div className={`bg-[url(/images/tarek.png)] bg-cover bg-center h-[80vh] ${utilStyles.bentoRectangle}`}></div>
+        <div className={`bg-[url(/images/tarek.png)] bg-cover bg-center h-[500px] ${utilStyles.bentoRectangle}`}></div>
 
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
