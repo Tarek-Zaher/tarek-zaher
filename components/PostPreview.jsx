78bfeb3b63091543a9fa56c221ba244b43c23526
tarek5142-godaddy <tarek5142@godaddy.com>
2025-07-12 13:59:26 -0500

    fixed transition background color

--- components/PostPreview.jsx
+++ components/PostPreview.jsx
@@ -5,63 +5,66 @@ import { libreBaskervilleRegular, latoRegular } from '../components/layout';
 import { colorPalette } from '../lib/colorPalette';
 
 export default function PostPreview({ postData, height, overlap }) {
-  const ref = useRef();
-  const router = useRouter();
+    const ref = useRef();
+    const router = useRouter();
+    const postColor = colorPalette[postData.color] || '#95B8D1';
 
-  const handleClick = () => {
-    const el = ref.current;
-    const rect = el.getBoundingClientRect();
+    const handleClick = () => {
+        const el = ref.current;
+        const rect = el.getBoundingClientRect();
 
-    // Clone element for animation
-    const clone = el.cloneNode(true);
-    clone.style.position = 'fixed';
-    clone.style.left = `${rect.left}px`;
-    clone.style.top = `${rect.top}px`;
-    clone.style.width = `${rect.width}px`;
-    clone.style.height = `${rect.height}px`;
-    clone.style.zIndex = 9999;
-    clone.style.margin = 0;
+        // Clone element for animation
+        const clone = el.cloneNode(true);
+        clone.style.position = 'fixed';
+        clone.style.left = `${rect.left}px`;
+        clone.style.top = `${rect.top}px`;
+        clone.style.width = `${rect.width}px`;
+        clone.style.height = `${rect.height}px`;
+        clone.style.zIndex = 9999;
+        clone.style.margin = 0;
 
-    document.body.appendChild(clone);
-    const cloneTitle = clone.querySelector('h2');
+        document.body.appendChild(clone);
+        const cloneTitle = clone.querySelector('h2');
 
-    gsap.to(cloneTitle, {
-        autoAlpha: 0,
-        duration: 0.3
-    });
-    gsap.to(clone, {
-      top: 0,
-      left: 0,
-      width: '100vw',
-      height: '100vh',
-      backgroundColor: postData.color,
-      borderRadius: 0,
-      ease: 'power2.inOut',
-      duration: 0.6,
-      onComplete: () => {
-        router.push(`/posts/${postData.id}`);
-      },
-    });
+        gsap.to(cloneTitle, {
+            autoAlpha: 0,
+            duration: 0.3
+        });
+        gsap.to(clone, {
+            top: 0,
+            left: 0,
+            width: '100vw',
+            height: '100vh',
+            borderRadius: 0,
+            ease: 'power2.inOut',
+            duration: 0.6,
+            onComplete: () => {
+                router.push(`/posts/${postData.id}`);
+            },
+        });
+        gsap.to(['html', 'body'], {
+            backgroundColor: postColor,
+            duration: 0.6,
+            ease: 'power2.inOut'
+        });
 
-    const removeClone = () => {
-      clone.remove();
-      router.events.off('routeChangeComplete', removeClone);
+        const removeClone = () => {
+            clone.remove();
+            router.events.off('routeChangeComplete', removeClone);
+        };
+
+        router.events.on('routeChangeComplete', removeClone);
     };
 
-    router.events.on('routeChangeComplete', removeClone);
-  };
-
-  const postColor = colorPalette[postData.color] || '#95B8D1';
-
-  return (
-    <div
-      ref={ref}
-      onClick={handleClick}
-      className={`post-preview prose text-[#181818] cursor-pointer mx-[20px] mb-[20px] ${overlap ? 'mt-[-150px]' : ''} border-0 rounded-[50px] ${libreBaskervilleRegular.className}`}
-      style={{ backgroundColor: postColor, height }}
-    >
-      <h5 className={`text-xs text- pt-3 px-[10px] pb-[10px] text-center uppercase ${latoRegular.className}`}>{postData.type}</h5>
-      <h2 className={`text-center text-[1.5rem] leading-[1.4] px-16 mt-2 font-normal`}>{postData.title}</h2>
-    </div>
-  );
+    return (
+        <div
+            ref={ref}
+            onClick={handleClick}
+            className={`post-preview prose text-[#181818] cursor-pointer mx-[20px] mb-[20px] ${overlap ? 'mt-[-150px]' : ''} border-0 rounded-[50px] ${libreBaskervilleRegular.className}`}
+            style={{ backgroundColor: postColor, height }}
+        >
+            <h5 className={`text-xs text- pt-3 px-[10px] pb-[10px] text-center uppercase ${latoRegular.className}`}>{postData.type}</h5>
+            <h2 className={`text-center text-[1.5rem] leading-[1.4] px-16 mt-2 font-normal`}>{postData.title}</h2>
+        </div>
+    );
 }
