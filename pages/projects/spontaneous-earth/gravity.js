f75da5b1434b25493237c9b3feda52b05a885038
Tarek-Zaher <Tarek21Zaher@gmail.com>
2026-06-06 16:09:02 -0500

    add gsap motion path svg animation

--- pages/projects/spontaneous-earth/gravity.js
+++ pages/projects/spontaneous-earth/gravity.js
@@ -1,9 +1,28 @@
+import { useRef } from 'react';
+import { gsap } from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { MotionPathPlugin } from 'gsap/MotionPathPlugin';
 
+gsap.registerPlugin(useGSAP, MotionPathPlugin);
 
 export default function Gravity() {
+    const container = useRef();
 
+    useGSAP(() => {
+        gsap.to("#mover", {
+            duration: 5,
+            repeat: 12,
+            yoyo: true,
+            ease: "none",
+            motionPath:{
+                path: "#path",
+                align: "#path",
+                alignOrigin: [0.5, 0.5],
+            }
+        })
+    }, {scope: container })
     return (
-        <div>
+        <div ref={container}>
             <div>
 
                 <h5 className="italic tracking-wide text-mist-700 text-center
@@ -26,15 +45,19 @@ export default function Gravity() {
                         How does gravity make things fall?
                     </h1>
                 
-               
-                <p className="
-                    text-mist-800 font-lato text-base
-                    w-xs md:w-xl mx-auto
-                ">
-                    <span class="uppercase text-lg font-normal">Spacetime </span>
-                    One of the great discoveries Einstein became famous for was his realization
-                    that we're moving through space as well as time. 
-                </p>
+               <div className="w-xs md:w-xl mx-auto">
+                    <p className="
+                        text-mist-800 font-lato text-base
+                    ">
+                        <span class="uppercase text-lg font-normal">Spacetime </span>
+                        One of the discoveries Einstein is famous for is the realization
+                        that time is a physical dimension we move through just like space. 
+                    </p>
+                    <svg className="mx-auto block" width="80%" height="100" viewBox="0 0 1000 100" preserveAspectRatio="xMidYMid meet">
+                        <path id="path" className="stroke-mist-800 stroke-2" fill="none" d="M10 50 H990"></path>
+                        <circle id="mover" r="10" cx="10" cy="50" className="fill-mist-800"></circle>
+                    </svg>
+                </div>
             </div>
         </div>
     )
