919a8becef6a7ae291a44bc10a3c0269775e566b
Tarek-Zaher <Tarek21Zaher@gmail.com>
2026-06-06 15:00:10 -0500

    add gravity page and add more font-weights for cormorant font

--- pages/projects/space.js
+++ pages/projects/space.js
@@ -13,29 +13,18 @@ export default function Stars() {
     return (
         <>
             <Starfield warpSpeedFactor={0.4} starCount={5000} starColor={[255, 255, 255]} />
-            <nav className="text-center mt-6">
-                <a href="/" 
-                className="text-white no-underline"></a>
-
+            <nav className="relative z-10 text-center mt-8 flex items-center justify-center">
+                <a href="/" className="text-white no-underline text-lg font-lexend">Spontaneous Earth</a>
             </nav>
             <div className="flex flex-col items-center justify-center h-screen text-center">
                 <h1 className=" 
                 text-mist-100 
                 text-6xl
+                font-normal
                 md:text-[10rem] 
                 font-libre-baskerville 
                 tracking-wide
-                ">Space</h1>
-                <h5 
-                className="
-                italic 
-                text-mist-100
-                text-xl
-                mx-auto
-                mt-16
-                max-w-2xl
-                "
-                ></h5>
+                ">Stars</h1>
             </div>
         </>
     )
---
+++ pages/projects/spontaneous-earth/gravity.js
@@ -0,0 +1,41 @@
+
+
+export default function Gravity() {
+
+    return (
+        <div>
+            <div>
+
+                <h5 className="italic tracking-wide text-mist-700 text-center
+                content-center w-xs md:w-xl mx-auto mt-32 mb-28">
+                    Despite what you may have been taught, high-mass objects like the Earth don't
+                    actually pull things towards them. Gravity isn't a force in the traditional sense. Instead, it's
+                    the result of the warping of spacetime by matter in the universe.
+                </h5>
+
+                <h1 className=" 
+                    text-mist-800 
+                    text-5xl text-center
+                    font-normal
+                    md:text-7xl
+                    font-cormorant
+                    tracking-wide
+                    w-xs md:w-xl flex-1
+                    content-center mx-auto mb-32
+                    ">
+                        How does gravity make things fall?
+                    </h1>
+                
+               
+                <p className="
+                    text-mist-800 font-lato text-base
+                    w-xs md:w-xl mx-auto
+                ">
+                    <span class="uppercase text-lg font-normal">Spacetime </span>
+                    One of the great discoveries Einstein became famous for was his realization
+                    that we're moving through space as well as time. 
+                </p>
+            </div>
+        </div>
+    )
+}
